8d7da4137e576d6db7eb7178028712ae7f69cc08
Chris Fritsen <chrisfritsen@gmail.com>
2023-12-16 16:57:18 -0800

    Fixed errors causing crashes. Errors were caused by syntax in Posts and Comments components when creating looped elements.

--- frontend/components/Comments/Comments.js
+++ frontend/components/Comments/Comments.js
@@ -8,7 +8,7 @@ const Comments = props => {
   return (
     <div className='comments-wrapper'>
       {/* map through the comments prop and render a Comment for every piece of data */}
-      {comments.map(com => Comment(com))}
+      {comments.map(com => <Comment comment={com} />)} 
     </div>
   );
 };
